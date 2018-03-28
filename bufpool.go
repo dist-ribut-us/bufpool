deb29dadf74ffb0ab7f489eb5c22375dfe3343cd
Adam Colton <adamcolton@gmail.com>
2018-03-27 21:14:54 -0400

    Extend BufferPool to make it easier to share pools while decoupling from the package.

--- bufpool.go
+++ bufpool.go
@@ -1,4 +1,3 @@
-// Package bufpool provides a pool of bytes.Buffer.
 package bufpool
 
 import (
@@ -6,19 +5,54 @@ import (
 	"sync"
 )
 
-var compressionBuffer = &sync.Pool{
-	New: func() interface{} {
-		return bytes.NewBuffer(nil)
+// BufferPool can Get or Put a Buffer to a pool
+type BufferPool interface {
+	Get() *bytes.Buffer
+	Put(buf *bytes.Buffer)
+}
+
+type bufferPool struct {
+	pool *sync.Pool
+}
+
+// Get returns a Buffer from the pool
+func (b *bufferPool) Get() *bytes.Buffer {
+	return b.pool.Get().(*bytes.Buffer)
+}
+
+// Put returns a buffer from the pool
+func (b *bufferPool) Put(buf *bytes.Buffer) {
+	buf.Reset()
+	b.pool.Put(buf)
+}
+
+// Pool is the package instance of BufferPool.
+var Pool BufferPool = &bufferPool{
+	pool: &sync.Pool{
+		New: func() interface{} {
+			return &bytes.Buffer{}
+		},
 	},
 }
 
-// Put a Buffer in the pool
-func Put(b *bytes.Buffer) {
-	b.Reset()
-	compressionBuffer.Put(b)
+// Get returns a Buffer from the pool
+func Get() *bytes.Buffer { return Pool.Get() }
+
+// Put returns a buffer from the pool
+func Put(buf *bytes.Buffer) { Pool.Put(buf) }
+
+// PutAndCopy returns the buffer to the pool and returns a copy of it's byte
+// slice
+func PutAndCopy(buf *bytes.Buffer) []byte {
+	bs := buf.Bytes()
+	cp := make([]byte, len(bs))
+	copy(cp, bs)
+	buf.Reset()
+	Pool.Put(buf)
+	return cp
 }
 
-// Get a Buffer from the pool
-func Get() *bytes.Buffer {
-	return compressionBuffer.Get().(*bytes.Buffer)
+// PutStr returns a buffer from the pool and returns it's value as a string
+func PutStr(buf *bytes.Buffer) string {
+	return string(PutAndCopy(buf))
 }
